6bc45e1e76c7f0787a45a88fee46f1fe1dafcfa2
SYALU ARINDYA SASKY <151634908+syaluar@users.noreply.github.com>
2024-12-24 18:49:00 +0700

    admin_dashboard.php

--- tubes/Database/admin_dashboard.php
+++ tubes/Database/admin_dashboard.php
@@ -58,3 +58,40 @@ $result = $conn->query($sql);
 <?php
 $conn->close();
 ?>
+*
+SQLyog Ultimate
+MySQL - 8.0.30 : Database - menu_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `menu_db`;
+
+/*Table structure for table `menu` */
+
+DROP TABLE IF EXISTS `menu`;
+
+CREATE TABLE `menu` (
+  `menu_id` int NOT NULL AUTO_INCREMENT,
+  `menu` varchar(255) NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `stock` int NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `menu` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
